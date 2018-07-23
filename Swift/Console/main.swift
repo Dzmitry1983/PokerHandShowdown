767cb5742e6dafc6c102a9327acd579e03d879f2
Dzmitry Kudrashou <dmitriy1983@gmail.com>
2018-07-23 14:38:50 -0500

    update Class Diagramm and add minor fix

--- Swift/Console/main.swift
+++ Swift/Console/main.swift
@@ -13,48 +13,6 @@
 import Foundation
 import PokerHandShowdown
 
-
-//MARK: - examples to use framework
-let playersOne = [
-	PlayerFactory.player(name:"Joe", cards:"8S", "8D", "AD", "QD", "JH")!,
-	PlayerFactory.player(name:"Bob", cards:"AS", "QS", "8S", "6S", "4S")!,
-	PlayerFactory.player(name:"Sally", cards:"4S", "4H", "3H", "QC", "8C")!,
-]
-
-let playersTwo = [
-	PlayerFactory.player(name:"Joe", cards:"QD", "8D", "KD", "7D", "3D")!,
-	PlayerFactory.player(name:"Bob", cards:"AS", "QS", "8S", "6S", "4S")!,
-	PlayerFactory.player(name:"Sally", cards:"4S", "4H", "3H", "QC", "8C")!,
-]
-
-let playersThree = [
-	PlayerFactory.player(name:"Joe", cards:"3H", "5D", "9C", "9D", "QH")!,
-	PlayerFactory.player(name:"Jen", cards:"5C", "7D", "9H", "9S", "QS")!,
-	PlayerFactory.player(name:"Bob", cards:"2H", "2C", "5S", "10C", "AH")!,
-]
-
-let playersFour = [
-	PlayerFactory.player(name:"Joe", cards:"2H", "3D", "4C", "5D", "10H")!,
-	PlayerFactory.player(name:"Jen", cards:"5C", "7D", "8H", "9S", "QD")!,
-	PlayerFactory.player(name:"Bob", cards:"2C", "4D", "5S", "10C", "JH")!,
-]
-
-let gameReduced = Game.init(false)
-let firstWinners = gameReduced.winners(from:playersOne)
-let secondWinners = gameReduced.winners(from:playersTwo)
-let thirstWinners = gameReduced.winners(from:playersThree)
-let fourstWinners = gameReduced.winners(from:playersFour)
-
-printPreGame(playersOne)
-printWinners(firstWinners)
-printPreGame(playersTwo)
-printWinners(secondWinners)
-printPreGame(playersThree)
-printWinners(thirstWinners)
-printPreGame(playersFour)
-printWinners(fourstWinners)
-
-
 //MARK: - support functions
 func cardToString(_ card:Card) -> String {
 	let suite:String
@@ -117,5 +75,48 @@ func printWinners(_ players:[Player]) {
 		print("\n")
 	}
 }
+
+//MARK: - examples to use framework
+let playersOne = [
+	PlayerFactory.player(name:"Joe", cards:"8S", "8D", "AD", "QD", "JH")!,
+	PlayerFactory.player(name:"Bob", cards:"AS", "QS", "8S", "6S", "4S")!,
+	PlayerFactory.player(name:"Sally", cards:"4S", "4H", "3H", "QC", "8C")!,
+]
+
+let playersTwo = [
+	PlayerFactory.player(name:"Joe", cards:"QD", "8D", "KD", "7D", "3D")!,
+	PlayerFactory.player(name:"Bob", cards:"AS", "QS", "8S", "6S", "4S")!,
+	PlayerFactory.player(name:"Sally", cards:"4S", "4H", "3H", "QC", "8C")!,
+]
+
+let playersThree = [
+	PlayerFactory.player(name:"Joe", cards:"3H", "5D", "9C", "9D", "QH")!,
+	PlayerFactory.player(name:"Jen", cards:"5C", "7D", "9H", "9S", "QS")!,
+	PlayerFactory.player(name:"Bob", cards:"2H", "2C", "5S", "10C", "AH")!,
+]
+
+let playersFour = [
+	PlayerFactory.player(name:"Joe", cards:"2H", "3D", "4C", "5D", "10H")!,
+	PlayerFactory.player(name:"Jen", cards:"5C", "7D", "8H", "9S", "QD")!,
+	PlayerFactory.player(name:"Bob", cards:"2C", "4D", "5S", "10C", "JH")!,
+]
+
+let gameReduced = Game.init(false)
+let firstWinners = gameReduced.winners(from:playersOne)
+let secondWinners = gameReduced.winners(from:playersTwo)
+let thirstWinners = gameReduced.winners(from:playersThree)
+let fourstWinners = gameReduced.winners(from:playersFour)
+
+printPreGame(playersOne)
+printWinners(firstWinners)
+printPreGame(playersTwo)
+printWinners(secondWinners)
+printPreGame(playersThree)
+printWinners(thirstWinners)
+printPreGame(playersFour)
+printWinners(fourstWinners)
+
+
+
 
 
